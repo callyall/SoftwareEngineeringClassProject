7959c11e85f1f571b55238870349b7894e17af7e
callyall <nikolay_brankov@outlook.com>
2018-01-29 22:29:17 +0200

    Update Event.java

--- src/main/java/com/softeng/ticket_application/model/Event.java
+++ src/main/java/com/softeng/ticket_application/model/Event.java
@@ -29,7 +29,7 @@ public class Event {
     private String location;
     @NotEmpty
     private String type;
-    @OneToMany(mappedBy="event")
+    @OneToMany(mappedBy="event",cascade = CascadeType.REMOVE, orphanRemoval = true)
     private List<Gate> gates;
 
     public Event() {
